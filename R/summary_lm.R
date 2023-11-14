c82f0a2fb9c9dcd1ef862e4792ac4685e451ee14
zhangjoy527 <zhangjoy@umich.edu>
2023-11-14 11:38:39 -0500

    Add comments to the summary_lm() function

--- R/summary_lm.R
+++ R/summary_lm.R
@@ -1,18 +1,35 @@
-# Hello, world!
+# summary_lm
 #
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
+# This function mimics the output of summary(lm()) by fitting a linear model.
+# It then returns the basic summary table of the summary() function, including
+# the OLS estimates, standard errors, t statistics, and p-values reported for
+# each beta value in our model
 #
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+summary_lm <- function(data, func) {
+  # Create the design matrix of the equation of interest, define the y variable
+  # given the data, the number of rows and the number of columns.
+  X <- model.matrix(func, data)
+  Y <- data[,c(toString(func[2]))]
+  n = nrow(X)
+  p = ncol(X)
+
+  # Solve for the beta estimates
+  estimate = solve(t(X) %*% X) %*% t(X) %*% Y
+
+  # Solve for the variance covariance matrix
+  Yhat = X %*% betahat
+  epsilonhat = Y - Yhat
+  sigma_squared = t(epsilonhat) %*% epsilonhat/(n-p)
+  var_betahat <- solve(t(X) %*% X) * c(sigma_squared)
+
+  # Solve for standard error of the estimates, as well as the t-statistics and
+  # the p-values
+  se <- sqrt(diag(var_betahat))
+  t_stat <- betahat / se
+  p_val <- 2*pt(t_stat, df = n - 2, lower.tail = F)
+
+  # Return a data frame containing the estimates, standard error, t-statistics,
+  # and p-value
+  data.frame(estimate, se, t_stat, p_val)
 }
