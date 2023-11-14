9641456186935f9478f1930ea7b666fac5bd9dc2
zhangjoy527 <zhangjoy@umich.edu>
2023-11-14 15:01:57 -0500

    changing name of betahat to estimate

--- R/summary_lm.R
+++ R/summary_lm.R
@@ -18,7 +18,7 @@ summary_lm <- function(data, func) {
   estimate = solve(t(X) %*% X) %*% t(X) %*% Y
 
   # Solve for the variance covariance matrix
-  Yhat = X %*% betahat
+  Yhat = X %*% estimate
   epsilonhat = Y - Yhat
   sigma_squared = t(epsilonhat) %*% epsilonhat/(n-p)
   var_betahat <- solve(t(X) %*% X) * c(sigma_squared)
@@ -26,7 +26,7 @@ summary_lm <- function(data, func) {
   # Solve for standard error of the estimates, as well as the t-statistics and
   # the p-values
   se <- sqrt(diag(var_betahat))
-  t_stat <- betahat / se
+  t_stat <- estimate / se
   p_val <- 2*pt(t_stat, df = n - 2, lower.tail = F)
 
   # Return a data frame containing the estimates, standard error, t-statistics,
